282d210d454e0b60f810b83348504791624390f6
sShauni <alann.emlopes@gmail.com>
2025-06-30 16:01:59 -0300

    etiquetas

--- ins.sh
+++ ins.sh
@@ -1,5 +1,8 @@
-
-
+sudo apt-get update
 sudo apt install python3 python3-tk
+sudo rm -rf LCD-show
+git clone https://github.com/goodtft/LCD-show.git
+chmod -R 755 LCD-show
+
 echo "
 Dependências instaladas..."
