d0e13f51be14ed1ec8da9aa8437e1645ac55941e
sShauni <alann.emlopes@gmail.com>
2025-07-07 16:11:26 -0300

    aaa

--- imp.sh
+++
@@ -1,8 +0,0 @@
-sudo install cups -y
-sudo usermod -aG lpadmin $USER
-sudo systemctl enable cups
-sudo systemctl start cups
-lpinfo -v
-
-echo "use lpadmin -p Thermal -E -v [uri] -m [ppd]"
-echo "ppd: lpadmin -m | grep [nome do fabricante. ex. zebra]"
