262ec1bc818e22eecf9f4327b097da1f4a65af98
sShauni <alann.emlopes@gmail.com>
2025-08-19 14:21:33 -0300

    imp setup changes

--- setup/imp.sh
+++ setup/imp.sh
@@ -2,7 +2,10 @@ sudo install cups -y
 sudo usermod -aG lpadmin $USER
 sudo systemctl enable cups
 sudo systemctl start cups
+echo ""
 lpinfo -v
-
+echo ""
+lpinfo -m | grep zebra
+echo ""
 echo "use lpadmin -p Thermal -E -v [uri] -m [ppd]"
-echo "ppd: lpadmin -m | grep [nome do fabricante. ex. zebra]"
+
