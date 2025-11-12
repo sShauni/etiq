2d1f90ab8494af3751f55881146f5a067ecf9ac6
sShauni <alann.emlopes@gmail.com>
2025-11-12 15:50:40 -0300

    novo setup

---
+++ setup novo/setup.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install python3-pip python3-dev libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libportmidi-dev libfreetype6-dev libavformat-dev libswscale-dev
+
+pip3 install pygame
